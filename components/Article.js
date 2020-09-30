ad2021f90b1b0746c249a7c728c78abee24eeb80
chazkiker2 <chazkiker@gmail.com>
2020-09-30 15:34:52 -0400

    complete article.js

--- components/Article.js
+++ components/Article.js
@@ -4,7 +4,9 @@
 import {data} from "./ArticleData";
 
 /*
-  * Step 1: Write a component called 'articleMaker' to create an article.
+  * COMPLETE
+  >> WORKING
+  Step 1: Write a component called 'articleMaker' to create an article.
   Your component is a function that takes an article object as its only argument,
   and returns a DOM node looking like the one below:
 
@@ -16,6 +18,17 @@ import {data} from "./ArticleData";
 
     <span class="expandButton">+</span>
   </div>
+
+  Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
+  This listener should toggle the class 'article-open' on div.article.
+
+  Step 3: Don't forget to return something from your function!
+
+  Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
+  to create a div.article element and append it to the DOM inside div.articles (see index.html).
+
+  Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
+  Refresh the page to see the new article.
 */
 const articleMaker = (article) => {
   // Instantiate Elements
@@ -49,32 +62,18 @@ const articleMaker = (article) => {
   span.textContent = "+";
 
   // span event listener 
-  span.addEventListener("click", (e) => {
+  span.addEventListener("click", () => {
     div.classList.toggle("article-open");
   });
   
   return div;
 };
-/* 
-  <div class="article">
-    <h2>{title of the article}</h2>
-    <p class="date">{date of the article}</p>
-    {three separate paragraph elements}
-    <span class="expandButton">+</span>
-  </div> 
-*/
+
+data.forEach(article => {
+  const articlesDiv = document.querySelector("div.articles");
+  articlesDiv.appendChild(articleMaker(article));
+});
 
 
-/*
 
-  Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
-  This listener should toggle the class 'article-open' on div.article.
 
-  Step 3: Don't forget to return something from your function!
-
-  Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
-  to create a div.article element and append it to the DOM inside div.articles (see index.html).
-
-  Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
-  Refresh the page to see the new article.
-*/
